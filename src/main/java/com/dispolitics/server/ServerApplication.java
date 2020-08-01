9c5bdc83c939c8cc32011cac9300dd1cfe6bfe1d
skylvsme <maksimhawkins@gmail.com>
2020-08-01 22:46:58 +0300

    Fixing internal server errors

--- src/main/java/com/dispolitics/server/ServerApplication.java
+++ src/main/java/com/dispolitics/server/ServerApplication.java
@@ -1,11 +1,8 @@
 package com.dispolitics.server;
 
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.security.auth.login.LoginException;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -15,14 +12,14 @@ public class ServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 
-        try {
+        /*try {
             jda = JDABuilder.createDefault("NzM5MDU5NTQxMzM3MTc4MTky.XyU81Q.stPRkENShdodBZJGK4UC9yf5fVM")
                     .addEventListeners(new MessageListener())
                     .build();
         } catch (LoginException e) {
             e.printStackTrace();
             return;
-        }
+        }*/
 	}
 
 }
--- src/main/java/com/dispolitics/server/models/City.java
+++ src/main/java/com/dispolitics/server/models/City.java
@@ -4,7 +4,6 @@ public class City {
 
     private Integer id;
 
-
     private String name;
     private Integer economyAmplifier;
     private Integer defendAmplifier;
