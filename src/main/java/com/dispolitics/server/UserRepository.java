61763428f79eb1ba064579d72b99fa5905b5bcfb
skylvsme <maksimhawkins@gmail.com>
2020-08-01 22:34:34 +0300

    Fixing error with bean name (still trying connect db)

--- src/main/java/com/dispolitics/server/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dispolitics.server;
-
-import com.dispolitics.server.models.City;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<City, Integer> {
-}
