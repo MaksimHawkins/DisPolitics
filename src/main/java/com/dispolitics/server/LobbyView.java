56cbc0859a88e3769fffe47ff6b6d36b40c7cc08
skylvsme <maksimhawkins@gmail.com>
2020-08-02 23:42:07 +0300

    Test lobby view

--- src/main/java/com/dispolitics/server/LobbyView.java
+++ src/main/java/com/dispolitics/server/LobbyView.java
@@ -1,8 +1,12 @@
 package com.dispolitics.server;
 
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
+import com.vaadin.flow.component.radiobutton.RadioGroupVariant;
 import com.vaadin.flow.router.Route;
 
 @Route("lobby")
@@ -10,20 +14,43 @@ public class LobbyView extends HorizontalLayout {
 
     public LobbyView() {
         setSizeFull();
-        setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
+
+        HorizontalLayout middleLayout = new HorizontalLayout();
+        middleLayout.setAlignItems(Alignment.CENTER);
+        middleLayout.setDefaultVerticalComponentAlignment(Alignment.END);
 
         VerticalLayout usersLayout = new VerticalLayout();
         usersLayout.getStyle()
                 .set("border" , "1px solid #ccc")
                 .set("border-radious", "3px");
-        usersLayout.setWidth("25%");
+        usersLayout.setWidth("100%");
+        usersLayout.setMaxWidth("450px");
 
         for (int i = 0; i < 5; i++) {
             usersLayout.add(userLayout("user" + (i + 1)));
         }
 
-        add(usersLayout);
+        middleLayout.add(usersLayout);
+
+        VerticalLayout countriesLayout = new VerticalLayout();
+        countriesLayout.setWidth("15%");
+        countriesLayout.getStyle()
+                .set("border" , "1px solid #ccc")
+                .set("border-radious", "3px");
+
+        RadioButtonGroup<String> countriesRadio = new RadioButtonGroup<>();
+        countriesRadio.setItems("Польша", "США", "Россия");
+        countriesRadio.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
+        countriesRadio.addValueChangeListener(event -> {
+            Notification.show(event.getValue());
+        });
+
+        countriesLayout.add(countriesRadio);
+
+        middleLayout.add(countriesLayout);
+
+        add(middleLayout);
     }
 
     public HorizontalLayout userLayout(String username) {
@@ -35,14 +62,14 @@ public class LobbyView extends HorizontalLayout {
         label.getStyle().set("font-size", "x-large");
         layout.add(label);
 
-        HorizontalLayout div = new HorizontalLayout();
+        Div div = new Div();
         div.getStyle()
                 .set("background", "lightgreen")
                 .set("border-radius", "50%");
         div.setWidth("40px");
-        div.setAlignSelf(Alignment.END);
 
         layout.add(div);
+        layout.expand(label);
 
         return layout;
     }
